8182def85524a73a64387bc9043ec46a619a0fb4
thiccnicc <nicolas.hanna@tum.de>
2023-11-14 15:51:13 +0100

    alexandra's nonsense
    Co-authored-by: radxelaan <radxelaan@users.noreply.github.com>

---
+++ BarnesHut.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <cmath>
+#include <TreeNode>
+
+class BarnesHut{
+    private:
+        TreeNode* root;
+
+    public:
+        BarnesHut(TreeNode node){
+            root = node
+        }
+
+        void insert(TreeNode node){
+            root.insertBody(node);
+        }
+}
---
+++ TreeNode.cpp
@@ -0,0 +1,47 @@
+#include<vector>
+#include<CelestialBody.cpp>
+
+using namespace std;
+
+class TreeNode{
+    private:
+        int totalMass;
+        vector<double> centerOfMass = vector<double>(3);
+        vector<TreeNode*> internal;
+        CelectialBody* external;
+
+    public:
+
+        //get octant
+
+        TreeNode(CelestialBody* ext){
+            external = ext;
+        }
+
+        void updateCenterOfMass(CelestialBody body){
+            vector<double> newCenterOfMass(3);
+            vector<double> bodyPosition = body.getPosition();
+            int bodyMass = body.getMass();
+            for (int i = 0; i < 3; i++){
+                newCenterOfMass[i] = (centerOfMass[i]*totalMass + bodyPosition[i]*bodyMass) / (totalMass + bodyMass); 
+            }
+            centerOfMass = newCenterOfMass;
+        }
+
+        void insertBody(TreeNode* node){
+            if (external){
+                internal = vector<TreeNode*>(8);
+                internal[getOctant(external)] = TreeNode(external);
+                internal[getOctant(node->external)] = node;
+                external = NULL;
+            }
+            else if (internal.size()){
+                internal[getOctant(node->external)] = node;
+                updateCenterOfMass(node->external);
+                totalMass += node->external.getMass();
+            }
+            else{
+                external = node->external;
+            }
+        }
+}
