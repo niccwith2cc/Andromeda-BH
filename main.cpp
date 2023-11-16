3fb7e833a29f6d48ad0c596a3159df2ad3d64781
thiccnicc <nicolas.hanna@tum.de>
2023-11-16 23:39:14 +0100

    making main more readable

--- main.cpp
+++ main.cpp
@@ -6,19 +6,14 @@
 #include "CelestialBody.cpp"
 using namespace std;
 
+// Hardcoding the Galaxy by taking a fixed number of bodies and generating their random positions and masses
+
 double generateRandomDouble(double min, double max) {
         random_device rd;
         mt19937 gen(rd());
         uniform_real_distribution<double> dis(min, max);
         return dis(gen);
     };
-
-// int generateRandomInt(int min, int max) {
-//         random_device rd;
-//         mt19937 gen(rd());
-//         uniform_real_distribution<int> dis(min, max);
-//         return dis(gen);
-//     };
 
 vector<double> generateRandomPosition(){
     vector<double> position(3);
@@ -30,15 +25,12 @@ vector<double> generateRandomPosition(){
 
 int main(){
 
-    //rand position could be of any value R^3 [-1,000,000:1,000,000]
-    //rand mass of Z [1*10^10:1*10^30]
-
-    int bodynumber =  6;
-
+    int bodynumber =  2;
     int mass;
     vector<double> position(3);
     vector<CelestialBody> bodies;
 
+    //generating the bodies using the CelestialBody class and giving them random masses and positions
     for (int i = 0; i < bodynumber; i++){
         mass = experimental::randint(500000,1000000);
         position = generateRandomPosition(); //check it out later
@@ -46,62 +38,35 @@ int main(){
         bodies.push_back(body);
     }
 
-    // cout << body1.CalcForce(body2) << endl;
-    // cout << body2.CalcForce(body3) << endl; 
-    // cout << body3.CalcForce(body1) << endl;
-    // cout << "the components of F:" << endl;
-    
-    // for (int i=0 ; i < 3 ; i++){
-    //     cout << body1.CalcComp(body2)[i] << "\t";
-    // }
-    
-    // cout << endl;
-
-    // cout << "the components of F:" << endl;
-    
-    // for (int i=0 ; i < 3 ; i++){
-    //     cout << body2.CalcComp(body3)[i] << "\t";
-    // }
-    // cout << endl;
-
-    // cout << "the components of F:" << endl;
-    
-    // for (int i=0 ; i < 3 ; i++){
-    //     cout << body3.CalcComp(body1)[i] << "\t";
-    // }
-    // cout << endl;
-
+    //Initializing the forces and acceleration total sum being a nx3 matrix
     vector<vector<double>> Fsum (bodies.size(), vector<double> (3,0));
     vector<vector<double>> Asum (bodies.size(), vector<double> (3,0));
-    cout << "the mass of the bodies:" << endl;
 
+    //for loop to calculate
     for (int i = 0; i < bodies.size(); i++){
         for (int j = 0; j < bodies.size(); j++){
             if (&bodies[i] != &bodies[j]){
                 vector<double> comp =  bodies[i].CalcCompF(bodies[j]);
+                vector<double> acomp =  bodies[i].CalcCompA(bodies[j]);
                 Fsum[i][0] += comp[0]; //the x components
                 Fsum[i][1] += comp[1]; //the y components
                 Fsum[i][2] += comp[2]; //the z components
-            }
-        }
-        cout << bodies[i].getMass() << " ";        
-    }
 
-    for (int i = 0; i < bodies.size(); i++){
-        for (int j = 0; j < bodies.size(); j++){
-            if (&bodies[i] != &bodies[j]){
-                vector<double> acomp =  bodies[i].CalcCompA(bodies[j]);
                 Asum[i][0] += acomp[0]; //the x components
                 Asum[i][1] += acomp[1]; //the y components
                 Asum[i][2] += acomp[2]; //the z components
             }
-        }
+        }     
     }
 
-    cout << endl;
-    cout << endl;
-    cout <<"The Forces:" << endl ;
-
+    //visualization and printing
+    cout << "The masses of the bodies: " << endl;
+    for (int i = 0 ; i <bodies.size(); i++){
+        cout << bodies[i].getMass() << " \t";
+    }
+        cout << endl;
+        cout << endl;
+        cout << "The Forces:" << endl ;
     for (int i = 0; i < bodies.size(); i++){
         for (int j = 0; j < 3; j++){
             cout << Fsum[i][j] << " \t";
@@ -109,8 +74,8 @@ int main(){
             cout << endl;
     }
     
-    cout << endl;
-    cout <<"The Acceleration:" << endl ;
+        cout << endl;
+        cout <<"The Acceleration:" << endl ;
 
     for (int i = 0; i < bodies.size(); i++){
         for (int j = 0; j < 3; j++){
@@ -119,8 +84,8 @@ int main(){
             cout << endl;
     }
 
-    cout << endl;
-    cout << "the positions: " << endl;
+        cout << endl;
+        cout << "the positions: " << endl;
     for (int i = 0; i < bodies.size(); i++){
         cout << "for body " << i << ": ";
         for (int j = 0; j < 3; j++){
@@ -129,8 +94,8 @@ int main(){
             cout << endl;
     }
 
-    cout << endl;
-    cout << "the position vectors: " << endl;
+        cout << endl;
+        cout << "the position vectors: " ;;
     for (int i = 0; i < bodies.size(); i++){
         for (int j = 0; j < bodies.size(); j++){
             if (&bodies[i] != &bodies[j]){
