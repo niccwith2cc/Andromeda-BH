3a5c561b68c9f772fb1b5fed50ca93c94115b0bc
thiccnicc <nicolas.hanna@tum.de>
2023-11-29 19:53:54 +0100

    vector hell

--- main.cpp
+++ main.cpp
@@ -141,30 +141,13 @@ int main(){
         vector<vector<vector<double>>> Vint (bodies.size(), vector<vector<double>> (3, vector<double> (time.size(),0.0)));
         vector<vector<vector<double>>> Pint (bodies.size(), vector<vector<double>> (3, vector<double> (time.size(),0.0)));
 
-        
-        for (int i = 0; i < bodies.size(); i++) {
-            for (int k = 0; k < time.size(); k++) {
-                Aint[i][0][k] = Asum[i][0];
-                Vint[i][0][0] = Aint[i][0][0]*timeStep;
-                Pint[i][0][0] = 0.5*Aint[i][0][0]*pow(timeStep,2);
-
-                Aint[i][1][k] = Asum[i][1];
-                Vint[i][1][0] = Aint[i][1][0]*timeStep;
-                Pint[i][1][0] = 0.5*Aint[i][1][0]*pow(timeStep,2);
-
-                Aint[i][2][k] = Asum[i][2];
-                Vint[i][2][0] = Aint[i][2][0]*timeStep;
-                Pint[i][2][0] = 0.5*Aint[i][2][0]*pow(timeStep,2);
-
-                Vint[i][0][k+1] = Aint[i][0][k]*timeStep + Vint[i][0][k];
-                // Vint[i][1][k] = Aint[i][1][k-1]*timeStep + Vint[i][1][k-1];
-                // Vint[i][2][k] = Aint[i][2][k-1]*timeStep + Vint[i][2][k-1];
-
-                Pint[i][0][k+1] = 0.5*Pint[i][0][k]*pow(timeStep,2) + Pint[i][0][k];
-                // Pint[i][1][k] = 0.5*Pint[i][1][k-1]*pow(timeStep,2) + Pint[i][1][k-1];
-                // Pint[i][2][k] = 0.5*Pint[i][2][k-1]*pow(timeStep,2) + Pint[i][2][k-1];
-
+        for (int i = 0; i < bodies.size(); i++){
+            for (int j = 0; j < 3; j++){
+                for (int k = 0; k < time.size(); k++){
+                    Aint.at(i).at(j).at(0) = Asum[i][j]; 
+                }
             }
+            
         }
 
         cout << "trial" << '\n';
