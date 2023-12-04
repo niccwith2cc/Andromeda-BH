5463243af44fdd66e6dc6a9ebb2ecc264e83ae61
Alexandra Apostolidou <go34fuh@tum.de>
2023-12-04 17:36:41 +0100

    code cleanup

--- main.cpp
+++ main.cpp
@@ -60,7 +60,7 @@ vector<vector<double>> calculateAcceleration(vector<CelestialBody> bodies){
 }
 
 
-    //Generating the bodies using the CelestialBody class and giving them random masses and positions
+//Generating the bodies using the CelestialBody class and giving them random masses and positions
 vector<CelestialBody> generateBodies(int bodynumber){
     vector<CelestialBody> bodies; 
     for (int i = 0; i < bodynumber; i++){
@@ -82,39 +82,6 @@ int main(){
     vector<CelestialBody> bodies = generateBodies(bodynumber);
     vector<vector<double>> Fsum = calculateForce(bodies);
     vector<vector<double>> Asum = calculateAcceleration(bodies);
-
-        // vector<double> pos1  = vector<double>{ 10.0, 10.0, 10.0 };
-        // vector<double> pos2  = vector<double>{ 100000.0, 100000.0, 100000.0 };
-        // vector<double> pos3  = vector<double>{ 900.0, -200.0, 30.0 };
-        // vector<double> pos4  = vector<double>{ -8490.0, -500.0, 30.0  };
-        // CelestialBody body1 = CelestialBody(500000, pos1, vector<double>(3), vector<double>(3), vector<double>(3));
-        // CelestialBody body2 = CelestialBody(500000, pos2, vector<double>(3), vector<double>(3), vector<double>(3));
-        // CelestialBody body3 = CelestialBody(555555, pos3, vector<double>(3), vector<double>(3), vector<double>(3));
-        // CelestialBody body4 = CelestialBody(4, pos4, vector<double>(3), vector<double>(3), vector<double>(3));
-
-        // BarnesHut tree = BarnesHut(&bodies[0]);
-        // for (int i = 1; i < bodynumber; i++) tree.insert(&bodies[i]);
-        // vector<double> force = tree.calculateForce(bodies[0], tree.root); 
-        // cout << force[0] << " " << force[1] << " " << force[2] << endl;
-        // cout << Fsum[0][0] << " " << Fsum[0][1] << " " << Fsum[0][2] << endl;
-
-        // vector<double> brute_force = body1.CalcCompF(body2); 
-        // cout << brute_force[0] << " " << brute_force[1] << " " << brute_force[2] << endl;
-
-        //root.insertBody(&body3);
-        //root.insertBody(&body4);
-        //root.insertBody(&bodies[2]);
-        //root.insertBody(&bodies[3]);
-        //cout << "Tree" << endl;
-        //root.traverseTree(&root);
-        //cout << root.centerOfMass[0] << " " << root.centerOfMass[1] << " " << root.centerOfMass[2] << endl;
-
-
-        //Calculating speed and distance from acceleration
-        //it is just taking the acceleration given and integrating over a specified time period, with additional intial velocity considered
-        //for now we assume vo and so are 0.  
-        // v = a*t + vo where t is the instant at which the acceleration is calculated.
-        // s = a*t^2/2 + vo*t + s0 
 
     vector<double> time;
     const double timeStep = 1.0 / 100.0; // Calculate the time step size    
@@ -152,9 +119,6 @@ int main(){
                 vector<double> pos = bodies[i].getPosition();
                 bodies[i].setPosition(pos + Pint[i]);
                 
-                //cout << "A " << Aint[i][0] << "\t" << Aint[i][1] << "\t" << Aint[i][2] << endl;
-                //cout << "V " << Vint[i][0] << "\t" << Vint[i][1] << "\t" << Vint[i][2] << endl;	
-                //cout << "P " << Pint[i][0] << "\t" << Pint[i][1] << "\t" << Pint[i][2] << endl;
             }
             //cout << "\n";
             for (int i = 0; i < bodies.size(); i++){
