9299bd4b363d2da22c1a90e6c10142679ab7550b
thiccnicc <nicolas.hanna@tum.de>
2024-01-11 14:35:22 +0100

    Revert "setting it to 90 bodies"
    This reverts commit 1a14d7fd7ce44e2a9304cf4fc89622cbb8828129.

--- visualization.py
+++ visualization.py
@@ -4,7 +4,7 @@ from matplotlib.animation import FuncAnimation
 import matplotlib; matplotlib.use("TkAgg")
 import csv
 
-FILENAME = 'build/pos.csv'
+FILENAME = 'pos.csv'
 BOUNDARY = 1000000
 positions = []
 
