7b1b76c7f6d48d6071a78c6633849139695be0fa
Alexandra Apostolidou <go34fuh@tum.de>
2024-02-03 15:23:42 +0100

    visualization 2.0

--- visualization.py
+++ visualization.py
@@ -7,6 +7,8 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib; matplotlib.use("TkAgg")
 import csv
 import configparser
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
 config = configparser.ConfigParser()
 config.read_file(open(r'config.ini'))
@@ -27,45 +29,12 @@ class Point:
 def grouped(iterable, n):
     return zip(*[iter(iterable)]*n)
 
-with open(FILENAME, newline='') as csvfile:
-
-    reader = csv.reader(csvfile, delimiter=',')
-    initial_positions = []
-    positions = []
-    first_row = next(reader)
-    for (x, y, z) in grouped(first_row, 3):
-        initial_positions.append(Point(x, y, z))
-    for row in reader:
-        positions.append([])
-        for (x, y, z) in grouped(row, 3):
-            positions[-1].append(Point(x, y, z))
-    positions = np.array(positions)
-    
-with open(FILENAME2, newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',')
-    masses = [float(mass) for mass in next(reader) if mass.strip()]
-    
-normalized_masses = [((mass - mass_minimum) / (mass_maximum - mass_minimum))*5 for mass in masses]
-
-def iteration(frame):
-    if frame < len(positions):
-        pos = positions[frame]
-    else:
-        pos = positions[-1]
-    x = []
-    y = []
-    z = []
-    for body in pos:
-        x.append(body.x)
-        y.append(body.y)
-        z.append(body.z)
-    ln.set_data_3d(x, y, z)
-    return ln,
 
 def init():
-    ax.set(xlim3d=(-5*BOUNDARY, 5*BOUNDARY), xlabel='X')
-    ax.set(ylim3d=(-5*BOUNDARY, 5*BOUNDARY), ylabel='Y')
-    ax.set(zlim3d=(-5*BOUNDARY, 5*BOUNDARY), zlabel='Z')
+    size_factor = 5
+    ax.set(xlim3d=(-size_factor*BOUNDARY, size_factor*BOUNDARY), xlabel='X')
+    ax.set(ylim3d=(-size_factor*BOUNDARY, size_factor*BOUNDARY), ylabel='Y')
+    ax.set(zlim3d=(-size_factor*BOUNDARY, size_factor*BOUNDARY), zlabel='Z')
     ax.xaxis.pane.fill = False
     ax.yaxis.pane.fill = False
     ax.zaxis.pane.fill = False
@@ -86,14 +55,53 @@ def init():
 
     ax.spines['left'].set_color('black')
     ax.spines['top'].set_color('black')
-    return ln,
 
 
-#fig, ax = plt.subplots()
-fig = plt.figure(figsize=(10, 10))
-ax = fig.add_subplot(projection="3d")
-xdata, ydata, zdata = [], [], []
-ln, = ax.plot([], [], [], c='red', marker="o", linestyle='', markersize=normalized_masses[0])
+def update_graph(frame):
+    if frame < len(positions):
+        pos = positions[frame]
+    else:
+        pos = positions[-1]
+    x = []
+    y = []
+    z = []
+    for body in pos:
+        x.append(body.x)
+        y.append(body.y)
+        z.append(body.z)
+    graph._offsets3d = (x, y, z)
 
-ani = FuncAnimation(fig, iteration, interval = 1, init_func=init, blit=True, cache_frame_data=False)
+with open(FILENAME, newline='') as csvfile:
+
+    reader = csv.reader(csvfile, delimiter=',')
+    initial_positions = []
+    positions = []
+    first_row = next(reader)
+    for (x, y, z) in grouped(first_row, 3):
+        initial_positions.append(Point(x, y, z))
+    for row in reader:
+        positions.append([])
+        for (x, y, z) in grouped(row, 3):
+            positions[-1].append(Point(x, y, z))
+    positions = np.array(positions)
+    
+with open(FILENAME2, newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    masses = [float(mass) for mass in next(reader) if mass.strip()]
+
+mass_scale = 20
+normalized_masses = [((mass - mass_minimum) / (mass_maximum - mass_minimum))*mass_scale for mass in masses]
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+initial_positions = positions[0]
+x = [pos.x for pos in initial_positions]
+y = [pos.y for pos in initial_positions]
+z = [pos.z for pos in initial_positions]
+
+graph = ax.scatter(x, y, z, c='b', marker='o', s=normalized_masses)
+
+ani = matplotlib.animation.FuncAnimation(fig, update_graph, init_func=init, interval=1, blit=False, cache_frame_data=False)
+
 plt.show()
